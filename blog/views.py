a24e00155b7b9bd49bfede699fcfe436c636cce2
Edwin Cubillos <edwin.cubillos.ext@nokia.com>
2020-01-17 10:00:54 -0300

    [Ad-CBV] :hammer: new questions, :wrench: create filters

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,26 @@
 from django.views.generic import TemplateView
+from django.views import generic
+
+from blog.models import Article
 
 
 class BlogPageView(TemplateView):
     template_name = 'blogs.html'
+
+
+class IndexView(generic.ListView):
+    context_object_name = 'articles'
+    template_name = 'latest_articles.html'
+
+    def get_queryset(self):
+        return Article.objects.order_by('-created_at')[:5]
+
+
+class DetailView(generic.DetailView):
+    """docstring for  DetailView"""
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class DeleteView(generic.DeleteView):
+    model = Article
